7fc631ecc32f9b2c80bc054e1d038a3ea3671e03
Mahmud Ahmad <duelwith_mahmud@live.com>
2017-12-12 12:19:50 -0500

    Initial Commit

---
+++ COMPORG_PROJECT2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COMPORG_PROJECT2
+//
+//  Created by Mahmud Ahmad on 12/12/17.
+//  Copyright © 2017 Mahmud Ahmad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
